33d931ef3b1d05eaed667dd3fe8deb25479807ad
Jacob Heider <jacob@tea.xyz>
2022-11-01 16:09:04 -0400

    inject maintainer into the metadata

--- .github/build-package-pages.sh
+++ .github/build-package-pages.sh
@@ -2,16 +2,17 @@
 # cmd: $ ./build-package-pages.sh srcJson targetPath
 # sample:  $ .github/build-package-pages.sh src/data/packages.json src/content/packages'
 
-for row in $(cat $1 | jq -r '.[] | @base64'); do
+for row in $(jq -r '.[] | @base64' < "$1"); do
     _jq() {
-     echo ${row} | base64 --decode | jq -r ${1}
+     echo "${row}" | base64 --decode | jq -r "${1}"
     }
-   touch $2/$(_jq '.slug').md
+   touch "$2"/"$(_jq '.slug')".md
    content="---
 type: page
 title: \"$(_jq '.name')\"
+maintainer: \"$(_jq '.maintainer')\"
 Description: \"$(_jq '.desc' | tr '\"' "'")\"
 layout: \"package-detail\"
 ---"
-   echo "$content" > $2/$(_jq '.slug').md
+   echo "$content" > "$2"/"$(_jq '.slug')".md
 done
