0cd7a5cba9836e0c4adbcd24515aa1a85bb72f29
neil <neil@neils-MacBook-Pro.local>
2022-12-07 13:07:00 +0800

    #215 apply more  lowered

--- .github/build-package-pages.sh
+++ .github/build-package-pages.sh
@@ -11,9 +11,10 @@ for row in $(jq -r '.[] | @base64' < "$1"); do
    if [[ "$full_name" == *\/* ]] || [[ "$full_name" == *\\* ]]
    then
       mkdir -p "$2/+$full_name"
+      # remove last folder
       rm -rf "$2/+$full_name"
    fi
-   echo $full_name
+
    touch "$2"/"+$full_name".md
    content="---
 type: page
@@ -22,5 +23,5 @@ maintainer: \"$(_jq '.maintainer')\"
 Description: \"$(_jq '.desc' | tr '\"' "'")\"
 layout: \"package-detail\"
 ---"
-   echo "$content" > "$2"/"+$(_jq '.full_name')".md
+   echo "$content" > "$2"/"+$full_name".md
 done
