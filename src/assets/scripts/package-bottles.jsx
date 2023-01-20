609e527c9a76d1fbdb74b1a44813cd4b21f84e98
Thomas Smith <49042513+tsmitty11@users.noreply.github.com>
2023-01-20 16:31:58 -0500

    Added styling to package-details page

--- src/assets/scripts/package-bottles.jsx
+++ src/assets/scripts/package-bottles.jsx
@@ -12,7 +12,14 @@ function PackageBottles() {
       });
   }, []);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return (
+    <div id="bottle-preloader" className="flex">
+      <div id="bottle-status" className="my-auto">
+        <i className="icon-tea-logo-iconasset-1 grid-gray tea-icon lead mb-0"></i>
+        <div id="bottle-loading-text"><p className="grid-gray">steeping available versions...</p></div>
+      </div>
+    </div>
+  );
 
   if (!bottles) return null;
 
